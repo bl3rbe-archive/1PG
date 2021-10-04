6f9e11079ae3a400725910b8e697d951c0b9c10f
Gaber Elsayed <47481084+muradmohamed@users.noreply.github.com>
2021-10-05 00:29:10 +0200

    Add files via upload

---
+++ dashboard/routes/root-routes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { commands } = require('../../handlers/command-handler');
+const bot = require('../../bot');
+const users = require('../../data/users');
+
+const router = express.Router();
+
+router.get('/', (req, res) => res.render('index'));
+
+router.get('/commands', (req, res) => res.render('commands', {
+  subtitle: 'Commands',
+  categories: [
+    { name: 'Auto Mod', icon: 'fas fa-gavel' },
+    { name: 'Economy', icon: 'fas fa-coins' }, 
+    { name: 'General', icon: 'fas fa-star' },
+    { name: 'Music', icon: 'fas fa-music' }
+  ],
+  commands: Array.from(commands.values()),
+  commandsString: JSON.stringify(Array.from(commands.values()))
+}));
+
+router.get('/leaderboard/:id', async (req, res) => {
+  const guild = bot.guilds.cache.get(req.params.id);
+  if (!guild)
+    return res.render('errors/404');
+
+  const savedUsers = (await users.getInGuild(req.params.id))
+    .sort((a, b) => (a.coins > b.coins) ? 1 : -1)
+    .slice(0, 100);
+
+  res.render('dashboard/leaderboard', { guild, savedUsers });
+});
+
+module.exports = router;
---
+++ handlers/events/message-event.js
@@ -0,0 +1,19 @@
+const Event = require('./event');
+const { handleCommand } = require('../command-handler');
+const logs = require('../../data/logs');
+const economy = require('../../modules/economy');
+
+module.exports = class extends Event {
+  on = 'message';
+  
+  async invoke(msg) {
+    if (!msg.guild || msg.author.bot) return;
+
+    const command = await handleCommand(msg);
+    if (command)
+      return await logs.add(msg.guild.id, 'commands');
+    
+    await logs.add(msg.guild.id, 'messages');
+    await economy.handleMessage(msg);
+  }
+}
